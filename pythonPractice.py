726071d08c85da233d5a4dc50574d9eb25871045
ab58 <arjunbhalla675@gmail.com>
2017-09-27 13:14:12 -0400

    Update pythonPractice.py

--- pythonPractice.py
+++ pythonPractice.py
@@ -12,34 +12,34 @@ def who_won_ttt(list_a):
 			break
 		x = list_a[row][0]
 		#print "x is "+str(x)
-		if x==0:
+		if x == 0:
 			continue
-		for col in range(1,len(list_a[x])):
+		for col in range(1, len(list_a[x])):
 			y = list_a[row][col]
 			#print "y is "+str(y)
-			if y!=x:
+			if y != x:
 				break
-			elif col==len(list_a[col])-1:
-				print "Player "+str(x)+" is the winner!"
+			elif col == len(list_a[col])-1:
+				print "Player " + str(x) + " is the winner!"
 				winner_found = True
 				break
 				
 	#next find vertical with identical entries (non-zero)
 	if not winner_found:
-		for col in range(0,len(list_a[0])):
+		for col in range(0, len(list_a[0])):
 			if winner_found:
 				break
 			x = list_a[0][col]
 			#print "x is "+str(x)
-			if x==0:
+			if x == 0:
 				continue
-			for row in range(1,len(list_a)):
+			for row in range(1, len(list_a)):
 				y = list_a[row][col]
 				#print "y is "+str(y)
-				if y!=x:
+				if y != x:
 					break
-				elif row==len(list_a)-1:
-					print "Player "+str(x)+" is the winner!"
+				elif row == len(list_a)-1:
+					print "Player " + str(x) + " is the winner!"
 					winner_found = True
 					break
 					
@@ -49,13 +49,13 @@ def who_won_ttt(list_a):
 	if not winner_found:
 		x = list_a[0][0]
 		#print "x is "+str(x)
-		if x!=0:
-			for i in range(1,len(list_a)):
+		if x != 0:
+			for i in range(1, len(list_a)):
 				y = list_a[i][i]
 				#print "y is "+str(y)
-				if y!=x:
+				if y != x:
 					break
-				elif i==len(list_a)-1:
+				elif i == len(list_a)-1:
 					print "Player "+str(x)+" is the winner!"
 					winner_found = True
 					break
@@ -64,14 +64,14 @@ def who_won_ttt(list_a):
 	if not winner_found:
 		x = list_a[0][len(list_a)-1]
 		#print "x is "+str(x)
-		if x!=0:
-			for i in range(1,len(list_a)):
+		if x != 0:
+			for i in range(1, len(list_a)):
 				y = list_a[i][len(list_a)-1-i]
 				#print "y is "+str(y)
-				if y!=x:
+				if y != x:
 					break
-				elif len(list_a)-1-i==0:
-					print "Player "+str(x)+" is the winner!"
+				elif len(list_a)-1-i == 0:
+					print "Player " + str(x) + " is the winner!"
 					winner_found = True
 					break
 					
@@ -89,7 +89,7 @@ def play_ttt(game_board):
 			coords1 = raw_input("Player 1 enter coordinates to play on (in format row,col): ")
 			row = int(coords1.split(",")[0])-1
 			col = int(coords1.split(",")[1])-1
-			if game_board[row][col]!=0:
+			if game_board[row][col] != 0:
 				print "Those coordinates have already been played. Please try again."
 			else:
 				game_board[row][col] = 1
@@ -105,26 +105,26 @@ def play_ttt(game_board):
 			coords2 = raw_input("Player 2 enter coordinates to play on (in format row,col): ")
 			row = int(coords2.split(",")[0])-1
 			col = int(coords2.split(",")[1])-1
-			if game_board[row][col]!=0:
+			if game_board[row][col] != 0:
 				print "Those coordinates have already been played. Please try again."
 			else:
 				game_board[row][col] = 2
 				valid_play = True
-				for i in range(0,len(game_board)):
+				for i in range(0, len(game_board)):
 					print game_board[i]
 					
-def max_of_three(x,y,z):
-	if x>y and x>z:
+def max_of_three(x, y, z):
+	if x > y and x > z:
 		return x
-	elif y>x and y>z:
+	elif y > x and y > z:
 		return y
-	elif z>x and z>y:
+	elif z > x and z > y:
 		return z
 	else:
 		return "2 or more of the 3 numbers are equal; no max found"
 
 def pick_word(f):
-	with open(f,"r") as open_file:
+	with open(f, "r") as open_file:
 		word_list = []
 		line = open_file.readline()
 		while line:
@@ -134,7 +134,7 @@ def pick_word(f):
 
 	
 def print_inc_word(incomplete_word):
-	for i in range(0,len(incomplete_word)):
+	for i in range(0, len(incomplete_word)):
 		print incomplete_word[i],
 	print ""
 def guess_letters(f):
@@ -143,7 +143,7 @@ def guess_letters(f):
 		word = pick_word(f)
 		incomplete_word = []
 		
-		for i in range(0,len(word)):
+		for i in range(0, len(word)):
 			incomplete_word.append("_")
 		print_inc_word(incomplete_word)
 		#print str(len(word))+" "+str(len(incomplete_word))
@@ -151,7 +151,7 @@ def guess_letters(f):
 		word_complete = False
 		penalty = 0
 		already_guessed = []
-		while not word_complete and penalty<6:
+		while not word_complete and penalty < 6:
 				
 			guess = raw_input("Guess your letter: ")
 		
@@ -163,25 +163,25 @@ def guess_letters(f):
 				already_guessed.append(guess)
 			
 			if guess in word:
-				for i in range(0,len(word)):
-					if word[i]==guess:
+				for i in range(0, len(word)):
+					if word[i] == guess:
 						incomplete_word[i] = guess
 				print_inc_word(incomplete_word)
 				if "_" not in incomplete_word:
 					word_complete = True
 					new_game = raw_input("You win! Start new game? (Y/N) ")
-					if new_game=="N":
+					if new_game == "N":
 						return
 			else:
 				print "Incorrect!"
-				penalty+=1
-				if penalty==5:
-					print "Only "+str(int(6-penalty))+" incorrect guess left!"
+				penalty += 1
+				if penalty == 5:
+					print "Only " + str(int(6 - penalty)) + " incorrect guess left!"
 				elif penalty<6:
-					print "Only "+str(int(6-penalty))+" incorrect guesses left!"
+					print "Only " + str(int(6 - penalty)) + " incorrect guesses left!"
 				else:
 					new_game = raw_input("You lose! Start new game? (Y/N) ")
-					if new_game=="N":
+					if new_game == "N":
 						return
 				print_inc_word(incomplete_word)
 				
@@ -191,11 +191,11 @@ def birthday_dictionary():
 	for name in b_dict.keys():
 		print name
 	name = raw_input("Whose birthday do you want to look up?\n")
-	print name+"'s birthday is "+b_dict[name]
+	print name + "'s birthday is " + b_dict[name]
 	
 
 def birthday_json(f):
-	with open(f,"r") as open_file:
+	with open(f, "r") as open_file:
 		b_dict = json.load(open_file)
 		
 	print "Welcome to the birthday dictionary. We know the birthdays of:"
@@ -203,21 +203,22 @@ def birthday_json(f):
 		print name
 		
 	name = raw_input("Whose birthday do you want to look up?\n")
-	print name+"'s birthday is "+b_dict[name]
+	print name + "'s birthday is " + b_dict[name]
 	
 	req = raw_input("Would you like to add another scientist's name and birthday to the dictionary? (Y/N) ")
-	if req=="Y":
+	if req == "Y":
 		new_name = raw_input("Great! Please enter the name of the scientist: ")
 		new_DOB = raw_input("Now enter their DOB (MM/DD/YYYY): ")
 		b_dict[new_name] = new_DOB
-		with open(f,"w") as open_file:
-			json.dump(b_dict,open_file)
+		with open(f, "w") as open_file:
+			json.dump(b_dict, open_file)
 			
 def birthday_months(f):
-	with open(f,"r") as open_file:
+	with open(f, "r") as open_file:
 		b_dict = json.load(open_file)
 	
-	birth_month_numbers = {"01":"January","02":"February","03":"March","04":"April","05":"May","06":"June","07":"July","08":"August","09":"September","10":"October","11":"November","12":"December"}
+	birth_month_numbers = {"01" : "January", "02" : "February", "03" : "March", "04" : "April", "05" : "May", 
+							"06" : "June", "07" : "July", "08" : "August", "09" : "September", "10" : "October", "11" : "November", "12" : "December"}
 	DOB_months = []
 	print "Welcome to the birthday dictionary. We know the birthdays of:"
 	for name in b_dict.keys():
@@ -229,17 +230,17 @@ def birthday_months(f):
 	print "Here are the birth months of scientists in the database and how many were born in each month:"
 	c = Counter(DOB_months)
 	print "{"
-	for k,v in c.most_common():
-		print "\t"+k+": "+str(v)
+	for k, v in c.most_common():
+		print "\t" + k + ": " + str(v)
 	print "}"
 	return c
 
 def bokeh_sample():
 	output_file("plot.html")
-	x = [10,20,30]
-	y = [4,5,6]
+	x = [10, 20, 30]
+	y = [4, 5, 6]
 	p = figure()
-	p.vbar(x=x,top=y,width=0.5)
+	p.vbar(x = x,top = y,width = 0.5)
 	show(p)
 	
 def birthday_plots(f):
@@ -248,22 +249,22 @@ def birthday_plots(f):
 	x = data.keys()
 	x_categories = data.keys()
 	y = data.values()
-	p = figure(x_range=x_categories) #This is to get a non-numerical x-axis value to work in the plot
-	p.vbar(x=x,top=y,width=0.5)
+	p = figure(x_range = x_categories) #This is to get a non-numerical x-axis value to work in the plot
+	p.vbar(x = x,top = y,width = 0.5)
 	show(p)
 	
 def main():
-	#who_won_ttt([[1,0,1],
-	#			 [2,2,2],
-	#			 [1,0,1]])
-	#play_ttt([[0,0,0],
-	#		  [0,0,0],
-	#		  [0,0,0]])
-	#print max_of_three(7,6,4)
-	#picked_word = pick_word("sowpods.txt")
-	#print picked_word
-	#print len(picked_word)
-	#guess_letters("sowpods.txt")
+	who_won_ttt([[1, 0, 1],
+				 [2, 2, 2],
+				 [1, 0, 1]])
+	play_ttt([[0, 0, 0],
+			  [0, 0, 0],
+			  [0, 0, 0]])
+	print max_of_three(7, 6, 4)
+	picked_word = pick_word("sowpods.txt")
+	print picked_word
+	print len(picked_word)
+	guess_letters("sowpods.txt")
 	#birthday_dictionary()
 	#birthday_json("birthday_dictionary.json")
 	#birthday_months("birthday_dictionary.json")
